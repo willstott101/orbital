76115ffba58a8bc536c08eb5a9c013a16e1fca52
Will Stott <Stott@mark-stotts-imac-5.local>
2014-08-09 17:34:04 +0100

    pixi dependency

--- package.js
+++ package.js
@@ -2,8 +2,15 @@ Package.describe({
   summary: "A HTML5 game engine, making use of both meteor, and PIXI."
 });
 
+//The order of this defines the order of file loading.
+//I think this is much better than using client/, lib/, etc.
+
+/*We have more control, and it allows us to structure our source in a more useful way for the engine.*/
 Package.on_use(function (api, where) {
-  api.add_files('orbital.js', ['client', 'server']);
+
+	api.add_files('/client/lib/pixi.js', 'client');
+
+	api.add_files('orbital.js', ['client', 'server']);
 });
 
 Package.on_test(function (api) {
